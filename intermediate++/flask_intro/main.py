3e3beb815054aac9c08d87fed439c906e3bf3726
Ankush <deshwalankush23@gmail.com>
2024-03-11 21:16:01 +0530

    Day 53: Data Entry Capstone Project (Zillow)

---
+++ intermediate++/flask_intro/main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask( __name__ )
+
+@app.route('/')
+def home():
+    return "Hello, Ankush"
+
+@app.route("/<name>")
+def hello_world( name ):
+    return f'Hello, {name}!'
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- intermediate++/instagram_bot/main.py
+++ intermediate++/instagram_bot/main.py
@@ -4,15 +4,14 @@ from selenium.webdriver.common.by import By
 from os import getenv
 from dotenv import  load_dotenv
 from time import sleep
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 
 load_dotenv("../../.env")
 
 EMAIL = getenv("INSTA_ID")
 PASSWORD = getenv("INSTA_PASSWORD")
 URL = "https://www.instagram.com/"
-related_id = "the.kaizenist"
+SIMILAR_ACCOUNT = "the.kaizenist"
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
@@ -34,18 +33,48 @@ class InstaFollower():
         sleep(2)
         password.send_keys(Keys.ENTER)
 
-        sleep(2)
-        # save_login_info = self.driver.find_elements(By.XPATH, value="// button[contains(text(), 'Not Now')]")
         
-        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@class='Fifk5']")))
+        sleep(4.3)
+        # save_login_prompt = self.driver.find_element(by=By.XPATH, value="//div[contains(text(), 'Not now')]")
+        # if save_login_prompt:
+        #     save_login_prompt.click()
 
-        notification_div = self.driver.find_element(By.XPATH, value="//div[@class='Fifk5']")
-        notification_div.click()
+        # sleep(3.7)
+        # notifications_prompt = self.driver.find_element(by=By.XPATH, value="//button[contains(text(), 'Not Now')]")
+        # if notifications_prompt:
+        #     notifications_prompt.click()
+
+        
     def find_followers(self):
-        pass
+        sleep(4)
+        self.driver.get(f"https://www.instagram.com/{SIMILAR_ACCOUNT}/followers")
+
+        sleep(4.2)
+        modal_xpath = "/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]"
+        modal = self.driver.find_element(by=By.XPATH, value=modal_xpath)
+        for i in range(5):
+
+            self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", modal)
+            sleep(2)
+
+
     def follow(self):
-        pass
+        all_buttons = self.driver.find_elements(By.CSS_SELECTOR, value='._aano button')
+
+        for button in all_buttons:
+            button.click()
+            sleep(1.1)
+
 
 bot = InstaFollower(chrome_options)
 bot.login()
+bot.find_followers()
+bot.follow()
 
+
+
+
+# In this case we're executing some Javascript, that's what the execute_script() method does.
+# The method can accept the script as well as an HTML element.
+# The modal in this case, becomes the arguments[0] in the script.
+# Then we're using Javascript to say: "scroll the top of the modal (popup) element by the height of the modal (popup)"
