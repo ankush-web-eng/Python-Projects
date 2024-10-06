f1e354c5252af7d86a347f8fd425107df706b213
Ankush <deshwalankush23@gmail.com>
2024-10-07 01:00:45 +0530

    Gold API updated

--- gold-api/app.py
+++ gold-api/app.py
@@ -1,41 +1,48 @@
 from flask import Flask, jsonify
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
+import requests
 from datetime import datetime
+from flask import Flask, jsonify
+from datetime import datetime
+import json
+from dotenv import load_dotenv
+import os
+
+
+# from bs4 import BeautifulSoup
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.common.by import By
+load_dotenv()
+
 
 app = Flask(__name__)
 
-def get_gold_price():
-    options = Options()
-    options.page_load_strategy = 'normal'
-    options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64)")
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_experimental_option('useAutomationExtension', False)
-    options.add_argument('--disable-blink-features=AutomationControlled')
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-infobars")
-    options.add_argument("--disable-extensions")
-    options.add_argument("--disable-popup-blocking")
-    options.add_argument('--ignore-certificate-errors-spki-list')
-    options.add_argument('--ignore-ssl-errors')
-    options.add_argument("--headless=new")
+def make_gapi_request():
+    api_key = os.getenv("GOLD_API_KEY")
+    symbol = "XAU"
+    curr = "INR"
+    date = ""
 
-    driver = webdriver.Chrome(options=options)
-
+    url = f"https://www.goldapi.io/api/{symbol}/{curr}{date}"
+    
+    headers = {
+        "x-access-token": api_key,
+        "Content-Type": "application/json"
+    }
+    
     try:
-        url = 'https://www.goodreturns.in/gold-rates/'
-        driver.get(url)
-        element = driver.find_element(By.XPATH, '//*[@id="moneyweb-leftPanel"]/setion/div/div[2]/div[2]/p')
-        gold_price = element.text
-    except Exception as e:
-        gold_price = None
-        print(f"Error: {e}")
-    finally:
-        driver.quit()
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
 
-    return gold_price
+        response_str = response.text
+        response_data = json.loads(response_str)
+        price_gram_24k = response_data.get("price_gram_24k")
+        print("Price per gram of 24K gold:", price_gram_24k)
+        return price_gram_24k
+    except requests.exceptions.RequestException as e:
+        print("Error:", str(e))
+        return None
+
 
 @app.route('/', methods=['GET'])
 def home():
@@ -43,8 +50,8 @@ def home():
 
 @app.route('/get-gold-price', methods=['GET'])
 def gold_price_api():
-    price = get_gold_price()
-    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # Capture current timestamp
+    price = make_gapi_request()
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     
     if price:
         return jsonify({
@@ -56,3 +63,37 @@ def gold_price_api():
             "error": "Unable to fetch the gold price",
             "timestamp": timestamp
         }), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# def get_gold_price():
+#     options = Options()
+#     options.page_load_strategy = 'normal'
+#     options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64)")
+#     options.add_experimental_option("excludeSwitches", ["enable-automation"])
+#     options.add_experimental_option('useAutomationExtension', False)
+#     options.add_argument('--disable-blink-features=AutomationControlled')
+#     options.add_argument("--no-sandbox")
+#     options.add_argument("--disable-dev-shm-usage")
+#     options.add_argument("--disable-infobars")
+#     options.add_argument("--disable-extensions")
+#     options.add_argument("--disable-popup-blocking")
+#     options.add_argument('--ignore-certificate-errors-spki-list')
+#     options.add_argument('--ignore-ssl-errors')
+#     options.add_argument("--headless=new")
+
+#     driver = webdriver.Chrome(options=options)
+
+#     try:
+#         url = 'https://www.goodreturns.in/gold-rates/'
+#         driver.get(url)
+#         element = driver.find_element(By.XPATH, '//*[@id="moneyweb-leftPanel"]/setion/div/div[2]/div[2]/p')
+#         gold_price = element.text
+#     except Exception as e:
+#         gold_price = None
+#         print(f"Error: {e}")
+#     finally:
+#         driver.quit()
+
+#     return gold_price
